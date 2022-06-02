2d988305834fe0f35b7a59f4020741c20617349f
gaessamzie-ncgn <gaessamzie@ncgn.co.kr>
2022-06-02 17:38:09 +0900

    hi-

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,29 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header></Header>
+    <main class="container">
+      <router-view></router-view>
+    </main>
+    <Footer></Footer>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue';
+import Footer from './components/Footer.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Header, Footer
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
+@import '@/assets/scss/base/fonts';
+@import '@/assets/scss/base/normalize';
+@import '@/assets/scss/base/reset';
+@import '@/assets/scss/base/typography';
+@import '@/assets/scss/layout/container';
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,11 @@
+<template>
+    <header class="header">
+        <h1 class="header__title">헤더</h1>
+    </header>
+</template>
+
+<script>
+export default {
+    name: 'Header'
+}
+</script>
